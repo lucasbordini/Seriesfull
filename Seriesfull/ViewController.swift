36c9c07171ab3f5514a143b16d006d92570936ef
Lucas Bordini <lucas.araujo@hanzo.com.br>
2022-05-18 13:12:40 -0300

    Initial Commit

---
+++ Seriesfull/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seriesfull
+//
+//  Created by lucasbordini on 18/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
